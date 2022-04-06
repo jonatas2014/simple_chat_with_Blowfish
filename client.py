1918c906d2b3e118ffd7611c71745457120f61bc
Gabriel San Martin <gssm.snf19@uea.edu.br>
2022-04-05 22:18:14 -0400

    Using RSA to share blowfish key

--- client.py
+++ client.py
@@ -1,39 +1,39 @@
 import argparse
 import cmd
 import pickle
+import rsa
 import socket
 import threading
 
-from constants import DEFAULT_KEY
 from criptography import BlowfishCriptography
 
 
 class User(cmd.Cmd, threading.Thread):
     prompt = 'Insert here: '
 
-    def __init__(self, connection, cipher):
+    def __init__(self, connection):
         cmd.Cmd.__init__(self)
         threading.Thread.__init__(self)
         self.connection = connection
-        self.cipher = cipher
         self.reader = connection.makefile('rb', -1)
         self.writer = connection.makefile('wb', 0)
+        
+        (public_key, private_key) = rsa.newkeys(512)
+        self.public_key = public_key
+        self.private_key = private_key
 
     def start(self):
-        """Begin execution of processor thread and user command loop."""
         super().start()
         super().cmdloop()
         self.cleanup()
 
     def cleanup(self):
-        """Close the connection and wait for the thread to terminate."""
         self.writer.flush()
         self.connection.shutdown(socket.SHUT_RDWR)
         self.connection.close()
         self.join()
 
     def run(self):
-        """Execute an automated message pump for client communications."""
         try:
             while True:
                 self.handle_server_command()
@@ -41,48 +41,44 @@ class User(cmd.Cmd, threading.Thread):
             pass
 
     def handle_server_command(self):
-        """Get an instruction from the server and execute it."""
-        source, encrypted_message = pickle.load(self.reader)
+        source, obj = pickle.load(self.reader)
 
-        message = None
-        try:
-            message = self.cipher.decrypt(encrypted_message)
-        except UnicodeDecodeError:
-            pass
+        if obj['code'] == '2':
+            message = None
+            try:
+                message = self.cipher.decrypt(obj['content'])
+            except UnicodeDecodeError:
+                pass
 
-        if message is None:
-            message = encrypted_message
+            if message is None:
+                message = obj['content']
 
-        print(f'\n{source[0]}: {message}')
+            print(f'\n{source[0]}: {message}')
+        else:
+            key = obj['content']
+            key = rsa.decrypt(key, self.private_key)
+            self.cipher = BlowfishCriptography(key)
 
     def preloop(self):
-        """Announce to other clients that we are connecting."""
-        message = f'{socket.gethostname()} just entered.'
-        message = self.cipher.encrypt(message)
-        pickle.dump(message, self.writer)
+        # Registering user
+        pickle.dump({'code': '1', 'content': self.public_key}, self.writer)
 
     def do_say(self, arg):
-        """Causes a message to appear to all other clients."""
-        message = self.cipher.encrypt(arg)
+        message = {'code': '2', 'content': self.cipher.encrypt(arg)}
         pickle.dump(message, self.writer)
         print(f'You: {arg}')
 
     def do_exit(self, arg):
-        """Disconnect from the server and close the client."""
         return True
 
     def postloop(self):
-        """Make an announcement to other clients that we are leaving."""
-        message = f'{socket.gethostname()} just exited.'
-        message = self.cipher.encrypt(message)
+        message = {'code': '2', 'content': self.cipher.encrypt(f'{socket.gethostname()} just exited.')}
         pickle.dump(message, self.writer)
 
 
 parser = argparse.ArgumentParser(description='Execute a chat client demo.')
 parser.add_argument('host', type=str, help='name of server on the network')
 parser.add_argument('port', type=int, help='location where server listens')
-parser.add_argument('key', type=str)
 arguments = parser.parse_args()
-cipher = BlowfishCriptography(key=arguments.key if arguments.key is not None else DEFAULT_KEY)
-client = User(socket.create_connection((arguments.host, arguments.port)), cipher)
+client = User(socket.create_connection((arguments.host, arguments.port)))
 client.start()
--- server.py
+++ server.py
@@ -1,51 +1,47 @@
 import argparse
 import pickle
 import queue
+import random
+import rsa
 import select
 import socket
 import socketserver
 
 
 class CustomServer(socketserver.ThreadingTCPServer):
-
-    """Provide server support for the management of connected clients."""
-
     def __init__(self, server_address, request_handler_class):
         """Initialize the server and keep a set of registered clients."""
         super().__init__(server_address, request_handler_class, True)
         self.clients = set()
+        self.key = self.__generate_random_key()
+
+    def __generate_random_key(self):
+        hash_ = random.getrandbits(128)
+        hash_ = '%032x' % hash_
+        return hash_
 
     def add_client(self, client):
-        """Register a client with the internal store of clients."""
         self.clients.add(client)
 
     def broadcast(self, source, data):
-        """Resend data to all clients except for the data's source."""
         for client in tuple(self.clients):
             if client is not source:
                 client.schedule((source.name, data))
 
     def remove_client(self, client):
-        """Take a client off the register to disable broadcasts to it."""
         self.clients.remove(client)
 
 
 class CustomHandler(socketserver.StreamRequestHandler):
-
-    """Allow forwarding of data to all other registered clients."""
-
     def __init__(self, request, client_address, server):
-        """Initialize the handler with a store for future date streams."""
         self.buffer = queue.Queue()
         super().__init__(request, client_address, server)
 
     def setup(self):
-        """Register self with the clients the server has available."""
         super().setup()
         self.server.add_client(self)
 
     def handle(self):
-        """Run a continuous message pump to broadcast all client data."""
         try:
             while True:
                 self.empty_buffers()
@@ -53,29 +49,30 @@ class CustomHandler(socketserver.StreamRequestHandler):
             pass
 
     def empty_buffers(self):
-        """Transfer data to other clients and write out all waiting data."""
         if self.readable:
-            self.server.broadcast(self, pickle.load(self.rfile))
+            data = pickle.load(self.rfile)
+            if data['code'] == '1':
+                public_key = data['content']
+                data['content'] = rsa.encrypt(self.server.key.encode('utf-8'), public_key)
+                self.schedule((self.name, data))
+            else:
+                self.server.broadcast(self, data)
         while not self.buffer.empty():
             pickle.dump(self.buffer.get_nowait(), self.wfile)
 
     @property
     def readable(self):
-        """Check if the client's connection can be read without blocking."""
         return self.connection in select.select(
             (self.connection,), (), (), 0.1)[0]
 
     @property
     def name(self):
-        """Get the client's address to which the server is connected."""
         return self.connection.getpeername()
 
     def schedule(self, data):
-        """Arrange for a data packet to be transmitted to the client."""
         self.buffer.put_nowait(data)
 
     def finish(self):
-        """Remove the client's registration from the server before closing."""
         self.server.remove_client(self)
         super().finish()
 
